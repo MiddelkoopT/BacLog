3ca4373db625a4211f5cf704aa4aae0923c3377e
t.middelkoop@ufl.edu <t.middelkoop@ufl.edu@e40f94fc-2c61-45b2-8b90-77fd4e96bf9e>
2010-11-03 21:18:36 +0000

    Multiple send overwrites message buffer.
    git-svn-id: https://webdss.ise.ufl.edu/svn/projects/BacLog/trunk@7 e40f94fc-2c61-45b2-8b90-77fd4e96bf9e

--- baclog.py
+++ baclog.py
@@ -8,14 +8,14 @@ import binascii
 
 def main():
     print "BacLog.main>"
-    ## Invoke 1, Read property, BO instance 20 (0x14), present-value (85).
+    ## Invoke 1{8}, Read property, BO instance 20 (0x14), present-value (85).
     message = "810a001101040003010c0c010000141955"
 
     s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     s.bind(('192.168.23.53',47808))
     s.setblocking(0)
 
-    send=1
+    send=3
     recv=send
     
     while(send+recv>0):
@@ -31,9 +31,9 @@ def main():
             send-=1
         ## Recv
         if sr and recv:
-            (message,source)=s.recvfrom(1500)
+            (response,source)=s.recvfrom(1500)
             ## Expect 810a0014010030010c0c0100001419553e91003f
-            print "BacLog.main> recv:", recv, source, binascii.b2a_hex(message)
+            print "BacLog.main> recv:", recv, source, binascii.b2a_hex(response)
             recv-=1
         if se:
             print "BacLog.main> error", se
