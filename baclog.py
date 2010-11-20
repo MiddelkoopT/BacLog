e65add15fcd2c7e67d880b2e3138d21ead3abb19
t.middelkoop@ufl.edu <t.middelkoop@ufl.edu@e40f94fc-2c61-45b2-8b90-77fd4e96bf9e>
2010-11-20 03:31:12 +0000

    Full packet decoding  * PhaseII decoding is setup by listing the properties and DataClass  * DataClass implements custom decoding  * Generic Tagged class to manage tags (pass last tag to next class)  * Packet has a generic "get next" data type  * PhaseI (Packets) need to be converted a some point.
    git-svn-id: https://webdss.ise.ufl.edu/svn/projects/BacLog/trunk@24 e40f94fc-2c61-45b2-8b90-77fd4e96bf9e

--- baclog.py
+++ baclog.py
@@ -17,15 +17,16 @@ import console as database
 BACnetObjectType = {'binary-input':3,'binary-output':4, 'device':8}
 BACnetPropertyIdentifier = {'present-value':85, 'object-list':76, 'notification-class':17}
 BACnetConfirmedServiceChoice = {'readProperty':12, 'subscribeCOV':5 }
+BACnetUnconfirmedServiceChoice = {'unconfirmedCOVNotification':2 }
 
 ## Used to get a new request ID
 LastRequestID = 0        
 
 class RawPacket:
     """Test packet class (send raw hex encoded packet)"""
-    def __init__(self,data=None,id=None):
+    def __init__(self,data=None,pid=None):
         self.data=data
-        self.id=id
+        self.pid=pid
         
     def __call__(self):
         return binascii.unhexlify(self.data)
@@ -36,6 +37,8 @@ class Packet:
         self._field=[]      # (name,value) of variable (None for constant/default)
         self._format=[]     # format of the field
         self._tag=[0]       # current context tag
+        self._data=None     # unparced data.
+        self._position=0    # beginning of unparced data.
         
         ## BVLC
         self._add(None,'B',0x81)            # BACnet/IP
@@ -98,6 +101,9 @@ class Packet:
             ## debug
             if(expected!=None and value!=expected):
                 print "Packet.decode> %s %02x %02x" %(name, value, expected)
+
+        self._data=data
+        self._position=packet.size
         #if(packet.size != self.length):
         #    print "Packet.decode> length", packet.size, self.length
 
@@ -157,25 +163,227 @@ class Packet:
     def _Enumerated(self,value,name=None):
         self._add(None,'B',0x91)        # Tag: Enumerated(9) | Application | Length (1)
         self._add(name,'B',value)       # Enumeration (0-255)
-
         
-class RequestPacket(Packet):
-    def __init__(self,servicechoice,id=None):
+    def _get(self,length=1):
+        """Return next slice of data"""
+        if self._position+length >= len(self._data):
+           return None
+        self._position+=length
+        return self._data[self._position-length:self._position]
+    
+
+## PhaseII Data types
+class Tagged:
+    def __init__(self,data=None,tag=None):
+        print "Tagged> %s " % self.__class__
+        self._tag=tag     ## Current tag.
+        self._value=None  ## Current "Value"
+        if data:
+            self._decode(data)
+            
+    def _getTag(self,data=None):
+        """Update current tag and return tag value, no data indicates last tag"""
+        if data==None:
+            return self._tag
+        tag=data._get()
+        if tag!=None:
+            tag,=struct.unpack('!B',tag)
+            #print "Tagged.getTag> 0x%02x" % tag, self._decodeTag(tag)
+        self._tag=tag
+        return tag
+        
+    def _decodeTag(self,tag=None):
+        if tag==None:
+            tag=self._tag # Default to current tag
+        num=(tag&0xF0)>>4
+        cls=(tag&0x08)>>3
+        lvt=(tag&0x07)
+        return num, cls, lvt
+    
+    def _openTag(self):
+        if self._tag==None:
+            return None
+        if (self._tag&0x0F)==0x0E:
+            return (self._tag&0xF0)>>4
+        
+    def _closeTag(self,data,tag=None):
+        """Returns True if more data until matched tag"""
+        if self._getTag(data)==None:
+            return None
+        if (self._tag&0x0F)!=0x0F:
+            return True
+        if tag!=None:
+            assert (self._tag>>4 == tag)
+        return False
+
+## Basic Types        
+
+class Unsigned(Tagged):
+    ## decode also used by Bitstring
+    def _decode(self, data):
+        num,cls,length=self._decodeTag()
+        assert length!=3 # unsupported unpack
+        self._value,=struct.unpack([None,'!B','!H'][length],data._get(length))
+        #print "Unsigned.decode>", self._value
+
+class Unsigned16(Unsigned):
+    pass
+
+class Unsigned32(Unsigned):
+    pass
+
+class Integer(Tagged):
+    pass
+
+class Enumerated(Unsigned):
+    def __str__(self):
+        self._display=dict((value, key) for key, value in self._enumeration.iteritems())
+        return "%s:%d" % self._display[self._value],self._value
+        
+class Bitstring(Tagged):
+    def _decode(self,data):
+        num,cls,length=self._decodeTag()
+        assert length!=(3+1) # unsupported unpack
+        self._unused,self._value=struct.unpack(['!BB','!BH'][length-2],data._get(length))
+
+class ObjectIdentifier(Tagged):
+    def _decode(self,data):
+        num,cls,length=self._decodeTag()
+        assert length==4
+        object,=struct.unpack('!L',data._get(length))
+        self.objectType=int((object&0xFFC00000)>>22)
+        self.instance=      (object&0x003FFFFF)
+        self._value=(self.objectType,self.instance)
+        #print "ObjectIdentifier.decode> %08x" % object , self._value
+
+## Composite types
+
+class Application(Tagged):
+    _application=[
+                  None,         # [A0] NULL
+                  None,         # [A1] Boolean
+                  Unsigned,     # [A2] Unsigned
+                  Integer,      # [A3] Integer
+                  None,         # [A4] Float
+                  None,         # [A5] Double
+                  None,         # [A6] Charactor
+                  None,         # [A7] Unicode
+                  Bitstring,    # [A8] Bitstring
+                  Enumerated,   # [A9] Enumerated
+                  ]
+    def _decode(self,data):
+        opentag=self._openTag()
+        
+        tag=self._getTag(data)
+        num,cls,lvt=self._decodeTag(tag)
+        DataClass = self._application[num]
+        element=DataClass(data,tag)
+        self._value=element._value
+        #print "Application.decode>", element._value
+
+        if opentag!=None:
+            self._closeTag(data,opentag)        
+
+class Sequence(Tagged):
+    def _decode(self,data):
+        opentag=self._openTag()
+        print "Sequence.decode> ############", opentag
+        while self._closeTag(data,opentag):
+            num,cls,lvt=self._decodeTag()
+            name, DataClass = self._sequence[num]
+            element=DataClass(data,self._getTag())
+            setattr(self,name,element._value)
+            print "Sequence.decode>", name, element._value
+        print "Sequence.decode> -----------", opentag
+        self._value=self
+
+class SequenceOf(Tagged):
+    def _decode(self,data):
+        ## Assume Context Tagging
+        opentag=self._openTag() 
+        print "SequenceOf.decode> ############", opentag
+        ## Only decode sequence of sequence
+        assert issubclass(self._sequenceof, Sequence)
+        self._value=[self._sequenceof()]
+        last=-1
+        while self._closeTag(data,opentag):
+            num,cls,lvt=self._decodeTag()
+            if num<=last: ## wrapped [strictly ordered tags]
+                self._value.append(self._sequenceof())
+            last=num
+            name, DataClass = self._sequenceof._sequence[num]
+            element=DataClass(data,self._getTag())
+            setattr(self._value[-1],name,element._value)
+            print "SequenceOf.decode>", len(self._value), name, element._value
+        print "SequenceOf.decode> ------------", opentag
+
+## PhaseII data types
+            
+class PropertyIdentifier(Enumerated):
+    ## TODO: Remove/Merge BACnetPropertyIdentifier
+    _enumeration={
+                  'present-value':85,
+                  'object-list':76,
+                  'notification-class':17
+                  }
+        
+class PropertyValue(Sequence):
+    _sequence=[
+               ('property',PropertyIdentifier),         # [0] propertyIdentifier
+               ('index',Unsigned),                      # [1] propertyArrayIndex OPTIONAL
+               ('value',Application),                   # [2] value ASN.1
+               ('priority',Unsigned),                   # [3] priority OPTIONAL
+               ]
+
+class SequenceOfPropertyValue(SequenceOf):
+    _sequenceof=PropertyValue
+
+## PhaseII PDU packets
+class COVNotification(Sequence): # SEQUENCE
+    _sequence=[
+               ('pid',Unsigned32),                      # [0] subscriberProcessIdentifier
+               ('device',ObjectIdentifier),             # [1] initiatingDeviceIdentifier
+               ('object',ObjectIdentifier),             # [2] monitoredObjectIdentifer
+               ('time',Unsigned),                       # [3] timeRemaining
+               ('values',SequenceOfPropertyValue),      # [4] listOfValues
+               ]
+    
+class ConfirmedCOVNotification(COVNotification):
+    pass
+class UnconfirmedCOVNotification(COVNotification):
+    pass
+    
+## PDU Requests
+BACnetConfirmedService =  { ConfirmedCOVNotification:1 } 
+BACnetUnconfirmedService =  { UnconfirmedCOVNotification:2 } 
+
+
+class ConfirmedRequest(Packet):
+    def __init__(self,servicechoice,pid=None):
         global LastRequestID
         Packet.__init__(self,0x04,0x00) # Expecting Reply ; Confirmed Request | Unsegmented
         self._add('segment','B',0x04)   # Maximum APDU size 1024
-        self._add('id','B',id)          # Request ID
+        self._add('pid','B',pid)        # Request ID
         self._add('servicechoice','B',  # serviceChoice/ACK [no tag]
                   BACnetConfirmedServiceChoice[servicechoice])
-        if id==None:
+        if pid==None:
             LastRequestID+=1
-            self.id=LastRequestID
+            self.pid=LastRequestID
+
+class UnconfirmedRequest(Packet):
+    def __init__(self,servicechoice=None,data=None):
+        global LastRequestID
+        Packet.__init__(self,0x00,0x10) # Not Expecting Reply ; Unconfirmed Request | Unsegmented
+        self._add('servicechoice','B',  # serviceChoice/ACK [no tag]
+                  servicechoice and BACnetUnconfirmedServiceChoice[servicechoice])
+        if(data):
+            self(data)
 
 class ComplexACK(Packet):
     def __init__(self,servicechoice=None,data=None):
         Packet.__init__(self,0x00,0x30) # Reply ; ComplexACK | Unsegmented
         ## APDU header
-        self._add('id','B',None)        # Responding to Request ID
+        self._add('pid','B',None)       # Responding to Request ID
         self._add('servicechoice','B',  # serviceChoice/ACK [no tag]
                   servicechoice and BACnetConfirmedServiceChoice[servicechoice])
         if(data):
@@ -183,16 +391,17 @@ class ComplexACK(Packet):
 
 class SimpleACK(Packet):
     def __init__(self,request=None,data=None):
-        Packet.__init__(self,0x00,0x20)      # Reply Present ; SimpleACK | 0x00
-        self._add('id','B',request and request.id)  # original request ID
+        Packet.__init__(self,0x00,0x20)                 # Reply Present ; SimpleACK | 0x00
+        self._add('pid','B',request and request.pid)    # original request ID
         self._add('servicechoice','B',request and request.servicechoice)  # serviceChoice/ACK
         if(data):
             self(data)
 
+## PDU Payload.
 
-class ReadPropertyRequest(RequestPacket):
+class ReadPropertyRequest(ConfirmedRequest):
     def __init__(self,type,instance,property='present-value'):
-        RequestPacket.__init__(self,'readProperty')
+        ConfirmedRequest.__init__(self,'readProperty')
         self._addObjectID(type,instance)    # ObjectIdentifier
         self._addPropertyID(property)       # PropertyIdentifier
         #                                   # PropertyArrayIndex (optional)
@@ -200,22 +409,21 @@ class ReadPropertyRequest(RequestPacket):
 class ReadPropertyResponse(ComplexACK):
     def __init__(self,request):
         ComplexACK.__init__(self,'readProperty')
-        self._addObjectID(request.type,request.id)  # ObjectIdentifier
-        self._addPropertyID(request.property)       # PropertyIdentifier
-        self._nextTag()                             # PropertyArrayIndex (Optional)
+        self._addObjectID(request.type,request.instance)    # ObjectIdentifier
+        self._addPropertyID(request.property)               # PropertyIdentifier
+        self._nextTag()                                     # PropertyArrayIndex (Optional)
         ## PropertyValue
         self._openTag()
         self._Enumerated(None,'value')
         self._closeTag()
 
-class SubscribeCOVRequest(RequestPacket):
+class SubscribeCOVRequest(ConfirmedRequest):
     def __init__(self,object,lifetime=120):
-        RequestPacket.__init__(self,'subscribeCOV')
+        ConfirmedRequest.__init__(self,'subscribeCOV')
         self._addUnsigned32(0)      # subscriberProcessIdentifier
         self._addObjectID(*object)  # monitoredObjectIdentifier
         self._addBoolean(False)     # issueConfirmedNotifications
         self._addUnsigned(lifetime) # lifetime
-
 
 #### Main Class
 
@@ -228,7 +436,7 @@ class BacLog:
         
         ## Load database driver
         self.db=database.Database()
-        print "Baclog>", self.config.get('Network','bind'), self.db.database
+        print "BacLog>", self.config.get('Network','bind'), self.db.database
         
         ## Work queues
         self.work={} # send/recv pair by invokeid
@@ -251,7 +459,7 @@ class BacLog:
         exit()
 
     def run(self):
-        print "Baclog.run>"
+        print "BacLog.run>"
 
         ## Read from database
         devices=self.db.getDevices();
@@ -268,16 +476,16 @@ class BacLog:
         #request=RawPacket("")
         #response=ResponsePacket(None,None)
         
-        self.work[request.id]=(request,response,target)
+        self.work[request.pid]=(request,response,target)
         self.send.append((request,target))
         
         ## subscribe to COV for 2 min.
         subscribe=SubscribeCOVRequest(object)
-        self.work[subscribe.id]=(subscribe,SimpleACK(subscribe),target)
+        self.work[subscribe.pid]=(subscribe,SimpleACK(subscribe),target)
         self.send.append((subscribe,target))
         
         self.process()
-        print "Baclog.run>", response.value
+        print "BacLog.run>", response.value
         self.shutdown()
 
     def process(self):
@@ -293,7 +501,7 @@ class BacLog:
             ## Send
             if sw and self.send:
                 (request,destination)=self.send.pop()
-                print "BacLog.process> send:", destination, request.id, request 
+                print "BacLog.process> send:", destination, request.pid, request 
                 s.sendto(request(),destination)
             ## Recv
             if sr:
@@ -307,12 +515,18 @@ class BacLog:
                 elif r.pdutype==0x2: ## SimpleACK
                     r=SimpleACK(data=response)
                 elif r.pdutype==0x1: ## Unconfirmed Request
-                    r=None ## Thinkgs get hard!
-                if r and self.work.has_key(r.id):
+                    r=UnconfirmedRequest(data=response)
+                    ## FIXME: Hard coded packet choice.
+                    if r.servicechoice==BACnetUnconfirmedServiceChoice['unconfirmedCOVNotification']:
+                        r=UnconfirmedRequest('unconfirmedCOVNotification',response)
+                        d=UnconfirmedCOVNotification(r)
+                        print d.values[0].property
+                        r=None
+                if r and self.work.has_key(r.pid):
                     # remove from work queue and put on done
-                    self.work[r.id][1](response)
-                    self.done.append(self.work[r.id])
-                    del self.work[r.id]
+                    self.work[r.pid][1](response)
+                    self.done.append(self.work[r.pid])
+                    del self.work[r.pid]
                 else:
                     # not work so put on data queue
                     self.data.append((response,source))
