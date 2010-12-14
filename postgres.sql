8f17d744389474a6fb16a7c570566ce960370326
t.middelkoop@ufl.edu <t.middelkoop@ufl.edu@e40f94fc-2c61-45b2-8b90-77fd4e96bf9e>
2010-12-14 13:38:43 +0000

    Update database schema
    git-svn-id: https://webdss.ise.ufl.edu/svn/projects/BacLog/trunk@39 e40f94fc-2c61-45b2-8b90-77fd4e96bf9e

--- postgres.sql
+++ postgres.sql
@@ -4,30 +4,42 @@
 -- BACnet device at the time of use (IP devices)
 DROP TABLE IF EXISTS Devices;
 CREATE TABLE Devices (
-	device SERIAL , -- device ID
+	deviceID SERIAL, -- device ID
 	IP inet,
 	port integer,
 	network integer, -- BACnet network number (should be the same)
 	instance integer, -- device instance (unique to network)
+	name varchar, -- device description
 	first timestamp, -- first seen
 	last timestamp, -- valid until (NULL indicates live object)
-	CONSTRAINT Devices_PK PRIMARY KEY (device)
+	CONSTRAINT Devices_PK PRIMARY KEY (deviceID)
 );
 
 -- Points.  Physical equipment maped to an object a point in time.
--- CREATE TABLE Points ( point integer -- Internal point ID );
+DROP TABLE IF EXISTS Points;
+CREATE TABLE Points ( 
+	pointID SERIAL, -- Internal point ID
+	name varchar, -- Full point name
+	building char(32), -- Building Name
+	room char(32), -- Room number/location
+	unit char(32), -- Sensor, actuator, or device name/number
+	description char(32), -- Description example: ROOM TEMP
+	first timestamp, -- first seen
+	last timestamp, -- valid until (NULL indicates live point)
+	CONSTRAINT Points_PK PRIMARY KEY (pointID)
+);
 
 -- BACnet points at the time of use.
 DROP TABLE IF EXISTS Objects;
 CREATE TABLE Objects (
-	object SERIAL, -- object ID; object definition (temporal)
-	device integer, -- device ID; device definition (temporal)
-	point integer, -- point ID; physical point definition
+	objectID SERIAL, -- object ID; object definition (temporal)
+	deviceID integer, -- device ID; device definition (temporal)
+	pointID integer, -- point ID; physical point definition
 	instance integer, -- object instance
 	type integer, -- BACnet ObjectType
 	first timestamp, -- first time seen, valid until last
 	last timestamp, -- last time seen (NULL indicates live object)
-	CONSTRAINT Objects_PK PRIMARY KEY (object)
+	CONSTRAINT Objects_PK PRIMARY KEY (objectID)
 );
 
 -- Log Data
